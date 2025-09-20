65bc14832b3c36af31991dbb9c788d10cb1c1d4c
Beryl Paece <berylmata@gmail.com>
2025-09-20 13:41:23 +0800

    feat: implemented python flask activity

---
+++ FLASK_LIBRARY/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restful import Api
+from models import db
+from controller import LibraryResource, LibraryListResource
+
+app = Flask(__name__)
+api = Api(app)
+
+# DB Configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.before_request
+def create_tables():
+    db.create_all()
+
+# Library routes
+api.add_resource(LibraryListResource, '/api/library')
+api.add_resource(LibraryResource, '/api/library/<string:isbn>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
